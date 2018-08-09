19a7083a64434c03905760cc1a341845c8579433
seva <seva.voloshin@gmail.com>
2018-08-10 01:52:35 +0300

    basic functionality added

---
+++ README.md
@@ -0,0 +1,4 @@
+# Fetch images from subreddit
+`const URL` contains path to subreddit's JSON.
+App skips `v.redd.it` and Imgur's `.gifv` contents.
+All of the images have full-size `src`.
--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const URL = "https://www.reddit.com/r/Delightfullychubby.json"
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: []
+    };
+  }
+
+  componentDidMount () {
+    fetch(URL).then(res => res.json())
+      .then(posts => {
+        this.setState({ posts: posts.data.children });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
   render() {
-    return (
-      <div></div>
-    );
+    let images = [];
+    for (var index = 0; index < this.state.posts.length; index++) {
+      var link = this.state.posts[index].data.url;
+      if((/.gifv/.test(link)) || (/v.redd.it/.test(link)))
+        continue;
+      if(/imgur/.test(link))
+        link += ".jpg";
+      images.push(<img src={link} alt={"image" + index} />);
+    }
+    return <div>{images}</div>;
   }
 }
 
