b81a25c40ba3fc26af8ca646e80821cb9b1ed644
Vsevolod Voloshyn <seva.voloshin@gmail.com>
2019-10-27 02:20:40 +0300

    Naming. Footer add.

--- src/App.js
+++ src/App.js
@@ -1,31 +1,37 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const URL = "https://www.reddit.com/r/Delightfullychubby.json?limit=50"
+const URL = "https://www.reddit.com/r/Delightfullychubby.json?limit=30";
+const THANKS = 'Thanks to the whole /r/Delightfullychubby community!';
+const THANKS_TIMEOUT = 10000;
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      posts: []
+      posts: [],
+      thanks: ''
     };
   }
 
   componentDidMount() {
     fetch(URL).then(res => res.json())
       .then(posts => {
-        this.addToStatePosts(posts.data.children)
+        this.addPostsToState(posts.data.children)
       })
+    setTimeout(() => {
+      this.setState({ thanks: THANKS });
+    }, THANKS_TIMEOUT);
   }
 
-  addToStatePosts(newPosts) {
+  addPostsToState(newPosts) {
     let filtered = newPosts.filter(value => {
       return value.data.url.match(/\.(jpeg|jpg|png)$/) != null;
     });
     this.setState({ posts: this.state.posts.concat(filtered) });
   }
 
-  renderImage(link) {
+  composeImage(link) {
     return (
       <a href={link} target="_blank">
         <img src={link} alt={"image"} />
@@ -35,7 +41,12 @@ class App extends Component {
 
   render() {
     return(
-      <div>{this.state.posts.map(post => this.renderImage(post.data.url))}</div>
+      <div>
+        {this.state.posts.map(post => this.composeImage(post.data.url))}
+        <span id="footer">
+          {this.state.thanks}
+        </span>
+      </div>
     );
   }
 }
